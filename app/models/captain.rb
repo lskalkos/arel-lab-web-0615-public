b2e3332b6c510d0e513d96f77dadc5b4503a7ad1
Lia Skalkos <hello@liaskalkos.net>
2015-07-09 10:46:00 -0400

    Completed AREL lab

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: :classifications).where(classifications: { name: 'Catamaran' })
+  end
+
+  def self.sailors
+    self.joins(boats: :classifications).where(classifications: { name: 'Sailboat' }).uniq
+  end
+
+  def self.talented_seamen
+    self.joins(boats: :classifications).where('classifications.name = "Sailboat" OR classifications.name = "Motorboat"').group('classifications.id')
+  end
+
+  def self.non_sailors
+    sailboat_captain_ids = self.sailors.pluck(:id)
+    self.where.not(id: sailboat_captain_ids)
+  end
+
 end
+
+
